30d3dbf4f77ac805bf9dfaddecb830a6cf0c5cc2
sankar <sankusanku001@gmail.com>
2023-12-06 22:47:17 +0000

    nav design completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { styles } from '../styles'
+import { navLinks } from '../constands'
+import { s_logo, menu, close } from '../assets'
 
 const Navbar = () => {
+
+  const [active, setActive] = useState("")
   return (
-    <div>Navbar</div>
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0  z-20 bg-primary`}>
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+          <Link 
+          to="/"
+          className='flex items-center gap-2'
+          onClick={()=> {
+            setActive("");
+            window.scroll(0, 0);
+          }}
+          >
+            <img src={s_logo} alt="logo" className='w-9 h-9 object-contain' />
+            <p className='text-white text-[18px] font-bold cursor-pointer'>Sankar</p>
+          </Link>
+          <ul className='list-none hidden sm:flex flex-row gap-10'>
+            {
+              navLinks.map((link)=>(
+                  <li>
+                    <a href={`#${link.id}`}>{link.title}</a>
+                  </li>
+              ))
+            }
+          </ul>
+      </div>
+    </nav>
   )
 }
 
