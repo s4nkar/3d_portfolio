3289d89d40a8d07738a4f2e5d9ce27f9ba7975ae
sankar <sankusanku001@gmail.com>
2024-01-15 03:14:33 +0000

    1st 3d model added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import { motion } from 'framer-motion'
+import { styles } from '../styles'
+import { ComputersCanvas } from './canvas'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='relative w-full h-screen mx-auto'>
+      <div
+      className={`
+      ${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5
+      `}
+      >
+        <div 
+        className='flex flex-col justify-center items-center mt-5'
+        >
+          <div className='w-5 h-5 rounded-full bg-[#9113ff]'/>
+          <div className='w-1  h-40 sm:h-80 violet-gradient'/>
+        </div>
+
+        <div>
+          <h1
+          className={`
+          ${styles.heroHeadText}
+          text-white
+          `}
+          >
+            Hi, I'am &nbsp;
+            <span className='text-[#915eff]'> 
+              Sankar
+            </span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+          Web developer integrating AI/ML <br className='hidden sm:block'/>for innovative and impactful solutions.
+          </p>
+        </div>
+      </div>
+
+        <ComputersCanvas/>
+
+
+    </section>
   )
 }
 
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import { Suspense, useEffect, useState } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
+
+import CanvasLoader from '../Loader'
 
 const Computers = () => {
-  return (
-    <div>Computers</div>
+
+  const computer = useGLTF('./desktop_pc/scene.gltf')
+
+  return ( 
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <pointLight intensity={1} color="#915eff"/>
+      <spotLight 
+      position={[-20, 50, 10]} 
+      color="#915eff"
+
+       />
+      <primitive
+        object={computer.scene}
+        scale={0.70}
+        position={[0, -3.25, -1.5]}
+        rotation={[-0.01, -0.02, -0.1]}
+      />
+    </mesh>
   )
 }
 
-export default Computers
+const ComputersCanvas = () =>{  
+  return( 
+    <Canvas
+    frameloop='demand'
+    shadows
+    camera={{ position:[20, 3, 5], fov: 25 }}
+    gl={{preserveDrawingBuffer: true}}
+    >
+
+      {/* <Suspense fallback={<CanvasLoader/>}> */}
+      <Suspense >
+        <OrbitControls 
+        enableZoom={false}
+        // maxPolarAngle={Math.PI / 2}
+        // minPolarAngle={Math.PI / 2}
+        enablePan={false}
+        />
+        <Computers/>
+      </Suspense>
+
+      <Preload all/>
+
+    </Canvas>
+  )
+}
+
+export default ComputersCanvas 
